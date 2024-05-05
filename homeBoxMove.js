35a881136a26204687fb70e9db61991fc7b25f7c
Dominick Couture <dominickcouture@dominicks-air-3.wifi.plymouth.edu>
2024-05-04 23:47:25 -0400

    first commit

---
+++ homeBoxMove.js
@@ -0,0 +1,7 @@
+console.log('test');
+let width = document.getElementById("txtBox");
+width.style.width = '35%';
+console.log(width.style.width);
+if (width < '500px'){
+    console.log('test word');
+}
---
+++ testJS.js
@@ -0,0 +1,6 @@
+console.log('something');
+let  elm = document.getElementById("some");
+let z = 100;
+
+elm.style.fontSize = z +"px";
+console.log(z);
